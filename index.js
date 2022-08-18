c137f8bff94ab5d4a12851f7070058d78db43dd8
Marwa <marwahakmi@gmail.com>
2022-08-18 12:49:02 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+const findMatching = (list, str) => {
+    const result = list.filter(item => item.toLowerCase() === str.toLowerCase())
+    return result
+}
+
+const fuzzyMatch = (list, param) => {
+    const result = list.filter(item => item.substring(0, param.length).toLowerCase() === param.toLowerCase())
+    return result
+}
+
+const matchName = (list, param) => {
+    const result = list.filter(item => item.name.toLowerCase() === param.toLowerCase())
+    return result
+}
+
+// Executions
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+console.log(fuzzyMatch(drivers, 'sa'))
